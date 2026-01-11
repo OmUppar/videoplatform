369b271b594c257969f732f6c103d4c4970ff7ca
OmUppar <omprakashuppar123@gmail.com>
2026-01-11 17:15:20 +0530

    Chnages For login process

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -95,7 +95,7 @@ export class AuthenticationService {
     const accessToken = this.jwtService.sign(payload, {
       // secret: process.env.JWT_SECRET as string,
       secret: 'mysecret',
-      // expiresIn: process.env.JWT_EXPIRES_IN as string,changes small
+      // expiresIn: process.env.JWT_EXPIRES_IN as string,
       expiresIn: '1d',
     });
 
